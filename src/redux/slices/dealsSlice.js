6bfd9a0f44441b11c5fdfacf24723c3561114b1d
aasuncion-fol <113270642+aasuncion-fol@users.noreply.github.com>
2023-04-09 01:05:26 -0400

    load data from firebase VehicleInventory collection

---
+++ src/redux/slices/dealsSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState =  {
+  deals: []
+}
+export const dealsSlice = createSlice({
+    name: 'deals',
+    initialState,
+    reducers: {
+      setDeals: (state, action) => {
+         state = action.payload
+         return state;
+      },
+      addDeal:(state, action)=>{
+        state.push({
+          id: action.payload.id,
+          description: action.payload.description,
+          done: action.payload.done
+        })
+      },
+
+    }
+});
+export const { setDeals,addDeal} = dealsSlice.actions;
+export default dealsSlice.reducer;
