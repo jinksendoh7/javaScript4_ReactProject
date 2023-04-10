73e3fd1f31e9a996f26cbd6301d9672ee130d12e
Jinky Castillote <jinksendoh7@gmail.com>
2023-04-09 23:09:33 -0400

    Vehicle form input fields initial

--- src/components/forms/vehicle/VehicleForm.js
+++ src/components/forms/vehicle/VehicleForm.js
@@ -1,28 +1,19 @@
-import { useState } from 'react';
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import { useState } from "react";
+import { Grid, TextField, Box } from "@mui/material";
 
-import CopyrightComponet from '../../copyright/CopyrightComponent';
-import {useNavigate} from 'react-router-dom';
-import * as auth from '../../../auth';
-import { useDispatch, useSelector } from 'react-redux';
-import { login, logout } from '../../../redux/slices/usersSlice';
-import { RoutesConst } from '../../../constants/AppConstants';
-
+import CopyrightComponet from "../../copyright/CopyrightComponent";
+import { useNavigate } from "react-router-dom";
+import * as auth from "../../../auth";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../../../redux/slices/usersSlice";
+import { RoutesConst } from "../../../constants/AppConstants";
 
 const VehicleForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -35,9 +26,9 @@ const VehicleForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     // Handle login logic here
-    console.log('in');
+    console.log("in");
     try {
-      const userAuth = await auth.login(email, password)
+      const userAuth = await auth.login(email, password);
       if (userAuth) {
         dispatch(
           login({
@@ -47,39 +38,122 @@ const VehicleForm = () => {
             photoUrl: userAuth.photoURL,
           })
         );
-        navigate(RoutesConst.ADMIN_ROUTE.concat('/',RoutesConst.ADMIN_DASHBOARD_ROUTE));
+        navigate(
+          RoutesConst.ADMIN_ROUTE.concat("/", RoutesConst.ADMIN_DASHBOARD_ROUTE)
+        );
       }
-
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
     }
   };
 
   return (
     <>
-       <Grid container spacing={{ xs: 1, md: 2, lg: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            <Grid item xs={2} sm={4} md={4}>
-              <div> 
-                <label>Trim</label>
-                <input type="text"/>
-              </div>
-              <div> 
-                <label>Model</label>
-                <input type="text"/>
-              </div>
-            </Grid>
-            <Grid item xs={2} sm={4} md={4}>
-              <div> 
-                <label>Trim</label>
-                <input type="text"/>
-              </div>
-              <div> 
-                <label>Model</label>
-                <input type="text"/>
-              </div>
-            </Grid>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 4 }}
+        columns={{ xs: 4, sm: 12, md: 8 }}
+      >
+        <Grid
+          item
+           xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Brand:" variant="outlined" />
         </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Model:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Year:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="VIN:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Exterior:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Type:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Fuel Type:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Transmission:" variant="outlined" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={4}
+          md={4}
+          sx={{
+            "& > :not(style)": { m: 1, width: "20ch" },
+          }}
+        >
+          <TextField label="Mileage:" variant="outlined" />
+        </Grid>
+      </Grid>
     </>
   );
 };
