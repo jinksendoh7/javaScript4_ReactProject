e2c8eda36289157bb5c8f53af5730480cab3bcce
Binfo3170 <113205045+Binfo3170@users.noreply.github.com>
2023-04-11 13:04:20 -0400

    Continued Update

--- src/pages/AdminPage/CustomersPage/CustomersPage.js
+++ src/pages/AdminPage/CustomersPage/CustomersPage.js
@@ -3,7 +3,6 @@ import CustomerOffers from "../../../components/customer-offers/CustomerOffers";
 const CustomersPage = () =>{
     return(
         <> 
-       Customer1
        <CustomerOffers />
         </>
 
